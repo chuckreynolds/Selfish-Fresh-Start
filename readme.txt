1ece6b65c61fe69fa174b07f16ce08443ab68092
chuckreynolds <chuck@vuurr.com>
2014-02-15 01:06:59 -0800

    update to 0.6, oop, cleanup, add char fixing, more

--- readme.txt
+++ readme.txt
@@ -1,17 +1,17 @@
 === Selfish Fresh Start ===
 Contributors: ryno267
 Donate link: http://rynoweb.com/wordpress-plugins/
-Tags: clean, fresh start, new install, clean admin, remove meta box, remove widgets, editor, profile fields, no smilies, no trackbacks, no pings, self pings, organize, declutter, clutter, theme editor, rsd links, wlw manifest links, shortlink, dashboard widgets, quick press, dashboard news, remove edit menu, remove editor, remove plugin editor, remove theme editor
+Tags: clean, fresh start, new install, clean admin, curly quotes, remove meta box, remove widgets, editor, profile fields, no smilies, no trackbacks, no pings, self pings, organize, declutter, clutter, theme editor, rsd links, wlw manifest links, shortlink, dashboard widgets, quick press, dashboard news, remove edit menu, remove editor, remove plugin editor, remove theme editor, aim, jabber, yim
 Requires at least: 3.0
-Tested up to: 3.5
-Stable tag: 0.5
+Tested up to: 3.8.1
+Stable tag: 0.6
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 Built to run on EVERY install you have, fresh start removes unneeded admin clutter and sets up the needed settings for every site.
 
 == Description ==
-This WordPress plugin Removes some, in my opinion, unused crappy dashboard, post & page widgets, checks for and nukes Hello Dolly, removes junk header tags including the generator tag for extra security, removes update notifications for non-admins, removes old user profile fields like aim, prevents self pinging, removes smilies and trackbacks, and a few other settings that nobody needs either. This is built to be very generalized so it'll work with every site as a good clean-up fresh start and help keep clients out of the edit menus.
+This WordPress plugin removes some, in my opinion, unused crappy dashboard, post & page widgets, fixes formatted curly quote problems, checks for and nukes Hello Dolly, removes junk header tags including the generator tag for extra security, removes update notifications for non-admins, removes old user profile fields like aim, prevents self pinging, removes smilies and trackbacks, and a few other settings that nobody needs either. This is built to be very generalized so it'll work with every site as a good clean-up fresh start and help keep clients out of the edit menus.
 
 = Current Operations =
 * Removed: clean up unneeded header tags including:
@@ -28,39 +28,42 @@ This WordPress plugin Removes some, in my opinion, unused crappy dashboard, post
 	* recent plugins feed
 	* wordpress development blog feed
 	* other wordpress blog news feed
+	* incoming links box
 * Removed: post metabox's
 	* trackbacks
-	* author *(if you must, use quick edit)*
-	* tags box *(nobody uses tags right, just not needed)*
 * Removed: page metabox's
 	* comments box
-	* author *(if you must, use quick edit)*
 	* discussion box
-* Removed: appearance menu theme editor *(some ppl may not like this but use ftp; great for client admins)*
+* Removed: appearance menu theme editor *(some ppl may not like this but it's great for client admins)*
 * Removed: plugins editor menu
 * Removed: plugins list edit links
 * Removed: more jump link to #anchor
 * Removed: update notifications for non-admin users
 * Removed: potential for self ping backs
-* Removed: checks for and nukes Hello Dolly plugin *(sorry Matt)*
+* Removed: checks for and nukes Hello Dolly plugin *(sorry @photomatt)*
 * Removed: admin user yim, aim, jabber fields
-* Add: user profile twitter, facebook, linkedin, google plus fields
 * Off: turn off global trackback/pingback setting
 * Off: turn off global formatting of text to graphic smilies
 
 == Installation ==
-
 1. Upload the `selfish-fresh-start` folder to the `/wp-content/plugins/` directory
 1. Activate the plugin
-1. That's it... seriously. Everything is done already. Enjoy.
+1. That's it. seriously. Everything is done already. Enjoy.
 
 == Changelog ==
+= 0.6 =
+* all object oriented now and cleaned up code a lot. mo betta
+* HUGE pet peeve of mine is people pasting from word (or others) with formatted text. so i'm tired of fixing it all the time so lets force fix curly quotes and some common unicode, ascii, and utf-8 problems
+* remove dashboard welcome panel that was added in 3.5
+* added back author metabox on posts only. seem to use this more often so back it goes
+* no longer adding user profile fields: twitter, facebook, linkedin, google plus. I took it out as most plugins now like Yoast SEO & Facebook's add those fields in order to do authorship verification and twitter cards etc. used to be needed but other plugins have finally caught up. time to take it out of here.
+
 = 0.5 =
 * checking if DISALLOW_FILE_EDIT is defined or not to avoid issues. if not add it.
 * comments div affecting some sites not able to turn them back on. spotty, so taking out for now
 
 = 0.4 =
-* temporarily removed removing 'slugdiv' from posts and pages as it was found to hinder the edit permalink updating function. that's another issue with wordpress but had to stop hiding that until wp fixes that.
+* temporarily removed removing 'slugdiv' from posts and pages as it was found to hinder the ajax edit permalink updating function. that's another issue with wordpress but had to stop hiding that until wp fixes that.
 
 = 0.3 =
 * added linkedin and google plus user fields
@@ -74,12 +77,12 @@ This WordPress plugin Removes some, in my opinion, unused crappy dashboard, post
 * take functions I use regularly and bundle for IPO *(initial public offering)*
 
 == Upgrade Notice ==
+= 0.6 =
+Some new features and cleaner code. overall a pretty big update.
 = 0.5 =
 Some sites reporting issues w/ comments always off on posts. put back comment div for now until more testing.
 = 0.2 =
 Added additional features, one of which helps secure your WP site. Always stay up to date!
 
-
 == Other Notes ==
-
-* Built on a hack-night at GangplankHQ.com. I always used a lot of these functions on every site to help clean up the admin stuff and do some basic settings and based on some twitter replies others wanted it as a plugin. So... here we go. Feel free to ask for features on my [Chuck Reynolds WordPress Plugins](http://rynoweb.com/wordpress-plugins/ "Chuck Reynolds WordPress Plugins") page.
+* Built in Chandler AZ, Updated in San Francisco, CA. I always used a lot of these functions on every site to help clean up the admin stuff and do some basic settings and based on some twitter replies others wanted this too as a public plugin. So... here we go. Feel free to ask for features on my [Chuck Reynolds WordPress Plugins](http://rynoweb.com/wordpress-plugins/ "Chuck Reynolds WordPress Plugins") page.
--- selfish-fresh-start.php
+++ selfish-fresh-start.php
@@ -2,145 +2,245 @@
 /*
 Plugin Name: Selfish Fresh Start
 Plugin URI: http://wordpress.org/extend/plugins/selfish-fresh-start
-Description: This WordPress plugin removes some, in my opinion, unused crappy dashboard, post & page widgets, checks for and nukes Hello Dolly, removes junk header tags, removes update notifications for non-admins, removes old user profile fields like aim, prevents self pining, removes smilies and trackbacks, and a few other settings that nobody needs either. This is built to be very generalized so it'll work with every site as a good clean-up fresh start.
-Version: 0.6b
+Description: This WordPress plugin removes some, in my opinion, unused crappy dashboard, post & page widgets, fixes formatted curly quote problems, checks for and nukes Hello Dolly, removes junk header tags including the generator tag for extra security, removes update notifications for non-admins, removes old user profile fields like aim, prevents self pinging, removes smilies and trackbacks, and a few other settings that nobody needs either. This is built to be very generalized so it'll work with every site as a good clean-up fresh start and help keep clients out of the edit menus.
+Version: 0.6
 Author: Chuck Reynolds
 Author URI: http://rynoweb.com/wordpress-plugins/
-License: GPL2
-*/
-/*
-	Copyright 2011 Selfish Fresh Start plugin (email: chuck@rynoweb.com)
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 2, as
-	published by the Free Software Foundation.
+Copyright 2014 Chuck Reynolds (email : chuck@rynoweb.com)
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
 
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*/
-/* changes from 0.5
-* remove dashboard welcome panel that was added in 3.5
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */
 
-add_action('after_setup_theme','rynonuke_setup');
-function rynonuke_setup() {
-	remove_action('wp_head','rsd_link');
-	remove_action('wp_head','wlwmanifest_link');
-	remove_action('wp_head','index_rel_link');
-	remove_action('wp_head','start_post_rel_link');
-	remove_action('wp_head','adjacent_posts_rel_link_wp_head');
-	remove_action('wp_head','wp_generator');
-	remove_action('wp_head','wp_shortlink_wp_head');
+Class RynoNuke {
 
-	add_action('admin_menu','rynonuke_dashboard_boxes');
-	add_action('admin_menu','rynonuke_post_metaboxes');
-	add_action('admin_menu','rynonuke_page_metaboxes');
-	add_action('admin_notices','rynonuke_update_notification_nonadmins',1);
-	add_action('pre_ping','rynonuke_self_pings');
-	add_action('admin_init','rynonuke_dolly');
-
-	add_filter('the_content_more_link','rynonuke_more_jump_link');
-	add_filter('user_contactmethods','rynonuke_contactmethods',10,1);
-	remove_action('welcome_panel','wp_welcome_panel'); // Remove welcome panel for all users
-}
-
-// remove dashboard widgets
-function rynonuke_dashboard_boxes() {
-	//remove_meta_box('dashboard_right_now','dashboard','core'); // right now overview box
-	//remove_meta_box('dashboard_incoming_links','dashboard','core'); // incoming links box
-	remove_meta_box('dashboard_quick_press','dashboard','core'); // quick press box
-	remove_meta_box('dashboard_plugins','dashboard','core'); // new plugins box
-	remove_meta_box('dashboard_recent_drafts','dashboard','core'); // recent drafts box
-	//remove_meta_box('dashboard_recent_comments','dashboard','core'); // recent comments box
-	remove_meta_box('dashboard_primary','dashboard','core'); // wordpress development blog box
-	remove_meta_box('dashboard_secondary','dashboard','core'); // other wordpress news box
-
-	// start removing plugin dashboard boxes. yup i'm goin there
-	remove_meta_box('yoast_db_widget','dashboard','core'); // yoasts dash news
-	remove_meta_box('aw_dashboard','dashboard','core'); // wp socializer box
-	remove_meta_box('w3tc_latest','dashboard','core'); // w3 total cache news box
-
-}
-// remove meta boxes from default posts screen
-function rynonuke_post_metaboxes() {
-	//remove_meta_box('postcustom','post','normal'); // custom fields metabox
-	//remove_meta_box('postexcerpt','post','normal'); // excerpt metabox
-	//remove_meta_box('commentstatusdiv','post','normal'); // comments metabox
-	remove_meta_box('trackbacksdiv','post','normal'); // trackbacks metabox
-	//remove_meta_box('slugdiv','post','normal'); // slug metabox (breaks edit permalink update)
-	remove_meta_box('authordiv','post','normal'); // author metabox
-	//remove_meta_box('revisionsdiv','post','normal'); // revisions metabox
-	remove_meta_box('tagsdiv-post_tag','post','normal'); // tags metabox
-	//remove_meta_box('categorydiv','post','normal'); // comments metabox
-}
-// remove meta boxes from default pages screen
-function rynonuke_page_metaboxes() {
-	//remove_meta_box('postcustom','page','normal'); // custom fields metabox
-	remove_meta_box('commentstatusdiv','page','normal'); // discussion metabox
-	remove_meta_box('commentsdiv','page','normal'); // comments metabox
-	//remove_meta_box('slugdiv','page','normal'); // slug metabox (breaks edit permalink update)
-	remove_meta_box('authordiv','page','normal'); // author metabox
-	//remove_meta_box('revisionsdiv','page','normal'); // revisions metabox
-	//remove_meta_box('postimagediv','page','side'); // featured image metabox
-}
-
-// remove update notifications for everybody except admin users
-function rynonuke_update_notification_nonadmins() {
-	if (!current_user_can('administrator'))
-		remove_action('admin_notices','update_nag',3);
-}
-
-// disable self-trackbacking
-function rynonuke_self_pings( &$links ) {
-    foreach ( $links as $l => $link )
-        if ( 0 === strpos( $link, get_option( 'home' ) ) )
-            unset($links[$l]);
-}
-
-// adios dolly
-function rynonuke_dolly() {
-    if (file_exists(WP_PLUGIN_DIR.'/hello.php')) {
-        delete_plugins(array('hello.php'));
-    }
-}
-
-// mod more link to not use hashtag anchor
-function rynonuke_more_jump_link($link) {
-	$offset = strpos($link,'#more-');
-	if ($offset) {
-		$end = strpos($link,'"',$offset);
+	/**
+	 * This is the constructor for RynoNuke Class
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		add_action('init', array($this, 'atInit'));
+		add_action('after_setup_theme', array($this, 'afterThemeSetup'));
 	}
-	if ($end) {
-		$link = substr_replace($link,'', $offset, $end-$offset);
+
+	/**
+	 * Functions called at the init action
+	 *
+	 * @return void
+	 */
+	public function atInit() {
+		$this->nukeFileEdit();
+		$this->nukeTrackbacksSmilies();
 	}
-	return $link;
+
+	/**
+	 * Functions called after the after_setup_theme action
+	 *
+	 * @return void
+	 */
+	public function afterThemeSetup() {
+		remove_action('wp_head', 'rsd_link');
+		remove_action('wp_head', 'wlwmanifest_link');
+		remove_action('wp_head', 'index_rel_link');
+		remove_action('wp_head', 'start_post_rel_link');
+		remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
+		remove_action('wp_head', 'wp_generator');
+		remove_action('wp_head', 'wp_shortlink_wp_head');
+		remove_action('welcome_panel', 'wp_welcome_anel');
+
+		add_action('admin_menu', array($this, 'nukeDashboardBoxes'));
+		add_action('admin_menu', array($this, 'nukePostMetaboxes'));
+		add_action('admin_menu', array($this, 'nukePageMetaboxes'));
+		add_action('admin_notices', array($this, 'nukeUpdateNotificationNonAdmins'), 1);
+		add_action('pre_ping', array($this, 'nukeSelfPings'));
+		add_action('admin_init', array($this, 'nukeHelloDolly'));
+
+		add_filter('the_content_more_link', array($this, 'nukeMoreJumpLinkAnchor'));
+		add_filter('user_contactmethods', array($this, 'nukeContactMethods'), 10, 1);
+		add_filter('content_save_pre', array($this, 'nukeCurlyOtherChars'));
+		add_filter('title_save_pre', array($this, 'nukeCurlyOtherChars'));
+	}
+
+	/**
+	 * Removes theme and plugin editor links if not defined already
+	 *
+	 * @return void
+	 */
+	public function nukeFileEdit() {
+		if ( ! defined('DISALLOW_FILE_EDIT') ) {
+	    	define('DISALLOW_FILE_EDIT', 'true');
+		}
+	}
+
+	/**
+	 * Sets db options table flags
+	 *
+	 * @return void
+	 */
+	public function nukeTrackbacksSmilies() {
+		update_option('default_ping_status', 'closed');
+		update_option('default_pingback_flag', '0');
+		update_option('use_smilies', '0');
+		# update_option('uploads_use_yearmonth_folders', '0');
+	}
+
+	/**
+	 * Removes some dashboard widgets
+	 *
+	 * @return void
+	 */
+	public function nukeDashboardBoxes() {
+		remove_meta_box('dashboard_incoming_links', 'dashboard', 'core'); // incoming links box
+		remove_meta_box('dashboard_quick_press', 'dashboard', 'core'); // quick press box
+		remove_meta_box('dashboard_plugins', 'dashboard', 'core'); // new plugins box
+		remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core'); // recent drafts box
+		remove_meta_box('dashboard_primary', 'dashboard', 'core'); // wordpress development blog box
+		remove_meta_box('dashboard_secondary', 'dashboard', 'core'); // other wordpress news box
+		# remove_meta_box('dashboard_right_now', 'dashboard', 'core'); // right now overview box
+		# remove_meta_box('dashboard_recent_comments', 'dashboard', 'core'); // recent comments box
+
+		// start removing plugin dashboard boxes. yup i'm goin there
+		remove_meta_box('yoast_db_widget', 'dashboard', 'core'); // yoasts dash news
+		remove_meta_box('aw_dashboard', 'dashboard', 'core'); // wp socializer box
+		remove_meta_box('w3tc_latest', 'dashboard', 'core'); // w3 total cache news box
+	}
+
+	/**
+	 * Removes some meta boxes from default posts screen
+	 *
+	 * @return void
+	 */
+	public function nukePostMetaboxes() {
+		# remove_meta_box('postcustom', 'post', 'normal'); // custom fields metabox
+		# remove_meta_box('postexcerpt', 'post', 'normal'); // excerpt metabox
+		# remove_meta_box('commentstatusdiv', 'post', 'normal'); // comments metabox
+		remove_meta_box('trackbacksdiv', 'post', 'normal'); // trackbacks metabox
+		# remove_meta_box('slugdiv', 'post', 'normal'); // slug metabox (breaks edit permalink update)
+		# remove_meta_box('authordiv', 'post', 'normal'); // author metabox
+		# remove_meta_box('revisionsdiv', 'post', 'normal'); // revisions metabox
+		# remove_meta_box('tagsdiv-post_tag', 'post', 'normal'); // tags metabox
+		# remove_meta_box('categorydiv', 'post', 'normal'); // comments metabox
+	}
+
+	/**
+	 * Removes some meta boxes from default pages screen
+	 *
+	 * @return void
+	 */
+	public function nukePageMetaboxes() {
+		# remove_meta_box('postcustom', 'page', 'normal'); // custom fields metabox
+		remove_meta_box('commentstatusdiv', 'page', 'normal'); // discussion metabox
+		remove_meta_box('commentsdiv', 'page', 'normal'); // comments metabox
+		# remove_meta_box('slugdiv', 'page', 'normal'); // slug metabox (breaks edit permalink update)
+		# remove_meta_box('authordiv', 'page', 'normal'); // author metabox
+		# remove_meta_box('revisionsdiv', 'page', 'normal'); // revisions metabox
+		# remove_meta_box('postimagediv', 'page', 'side'); // featured image metabox
+	}
+
+	/**
+	 * Removes update notifications for everybody except admin users
+	 *
+	 * @return void
+	 */
+	public function nukeUpdateNotificationNonAdmins() {
+		if ( ! current_user_can('administrator')) {
+			remove_action('admin_notices', 'update_nag', 3);
+		}
+	}
+
+	/**
+	 * Disables potential to self-trackback
+	 *
+	 * @return void
+	 */
+	public function nukeSelfPings(&$links) {
+	    foreach ($links as $l => $link) {
+	        if (0 === strpos($link, get_option('home'))) {
+	            unset($links[$l]);
+			}
+		}
+	}
+
+	/**
+	 * Removes hellodolly plugin if it exists. sorry @photomatt
+	 *
+	 * @return void
+	 */
+	public function nukeHelloDolly() {
+	    if (file_exists(WP_PLUGIN_DIR.'/hello.php')) {
+	        delete_plugins(array('hello.php'));
+	    }
+	}
+
+	/**
+	 * Modifies #more link to not use hashtag anchor
+	 *
+	 * @return void
+	 */
+	public function nukeMoreJumpLinkAnchor($link) {
+		$offset = strpos($link, '#more-');
+		if ($offset) {
+			$end = strpos($link, '"', $offset);
+		}
+		if ($end) {
+			$link = substr_replace($link, '', $offset, $end-$offset);
+		}
+		return $link;
+	}
+
+	/**
+	 * Removes obsolete profile fields
+	 *
+	 * @return void
+	 */
+	public function nukeContactMethods($contactMethods) {
+		unset($contactMethods['yim']);
+		unset($contactMethods['aim']);
+		unset($contactMethods['jabber']);
+
+		// removed in 0.6. see changelog for reason
+		# $contactMethods['rynonuke_twitter']='Twitter';
+		# $contactMethods['rynonuke_facebook']='Facebook';
+		# $contactMethods['rynonuke_googleplus']='Google +';
+		# $contactMethods['rynonuke_linkedin']='LinkedIn';
+
+		return $contactMethods;
+	}
+
+	/**
+	 * Fixes curly quotes and badly formatted characters. One of my bigger pet peeves is curly quotes from word pastes
+	 *
+	 * @return void
+	 */
+	public function nukeCurlyOtherChars($fixChars) {
+		$fixChars = str_replace(
+			array("\xe2\x80\x98", "\xe2\x80\x99", "\xe2\x80\x9c", "\xe2\x80\x9d", "\xe2\x80\x93", "\xe2\x80\x94", "\xe2\x80\xa6"),
+			array("'", "'", '"', '"', '-', '&mdash;', '&hellip;'), $fixChars);
+
+		$fixChars = str_replace(
+			array(chr(145), chr(146), chr(147), chr(148), chr(150), chr(151), chr(133), chr(38)),
+			array("'", "'", '"', '"', '-', '&mdash;', '&hellip;', '&amp;'), $fixChars);
+
+		$fixChars = str_replace(
+			array('â„¢', 'Â©', 'Â®'),
+			array('&trade;', '&copy;', '&reg;'), $fixChars);
+
+		return $fixChars;
+	}
 }
 
-// rem/add user profile fields
-function rynonuke_contactmethods($contactmethods) {
-	unset($contactmethods['yim']);
-	unset($contactmethods['aim']);
-	unset($contactmethods['jabber']);
-	$contactmethods['rynonuke_twitter']='Twitter';
-	$contactmethods['rynonuke_facebook']='Facebook';
-	$contactmethods['rynonuke_googleplus']='Google +';
-	$contactmethods['rynonuke_linkedin']='LinkedIn';
-	return $contactmethods;
-}
-
-// if not defined already, remove theme editor
-if(!defined('DISALLOW_FILE_EDIT')) {
-    define('DISALLOW_FILE_EDIT', 'true');
-}
-
-// options table flags
-update_option('default_ping_status','closed');
-update_option('default_pingback_flag','0');
-update_option('use_smilies','0');
-//update_option('uploads_use_yearmonth_folders','0');
+$rynonuke = new RynoNuke;
