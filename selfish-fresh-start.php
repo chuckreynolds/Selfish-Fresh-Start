628200ef50f6eadf9948e6668528c1d097c5db95
Chuck Reynolds <chuck@vuurr.com>
2017-03-21 00:14:13 -0700

    remove deprecated rel head calls
    index_rel_link & start_post_rel_link deprecated in v3.3.0

--- selfish-fresh-start.php
+++ selfish-fresh-start.php
@@ -93,8 +93,6 @@ if ( ! defined( 'WPINC' ) ) {
 
 		remove_action( 'wp_head',            'rsd_link' );
 		remove_action( 'wp_head',            'wlwmanifest_link' );
-		remove_action( 'wp_head',            'index_rel_link' );
-		remove_action( 'wp_head',            'start_post_rel_link' );
 		remove_action( 'wp_head',            'adjacent_posts_rel_link_wp_head' );
 		remove_action( 'wp_head',            'wp_generator' );
 		remove_action( 'wp_head',            'wp_shortlink_wp_head' );
